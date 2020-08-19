a5a37bdb1b4bc68b68d8b402adf9e78f456f4af8
SAMPATH <shyamalasampath484@gmail.com>
2020-08-19 16:18:47 +0530

    second commit

---
+++ src/test/java/Framework/project/Day1.java
@@ -0,0 +1,19 @@
+package Framework.project;
+
+import org.testng.annotations.Test;
+
+public class Day1 
+{
+@Test
+	public void loanssignin()
+	{
+		System.out.println("loans signin hello"+"URL");
+	}
+	
+	@Test
+	public void loanssignup()
+	{
+		System.out.println("loans signup bye ");
+	}
+
+}
